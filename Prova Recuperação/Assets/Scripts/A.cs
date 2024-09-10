0ec33421c46cc417050156be957f889e8a3075c5
Namizn <leleo2006@outlook.com>
2024-09-10 09:16:04 -0300

    asd
    asd

--- Prova Recuperação/Assets/Scripts/A.cs
+++ Prova Recuperação/Assets/Scripts/A.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class b : MonoBehaviour
+public class A : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
