60c9ce784f23def69f840c9c1a5fe15b1a4f2a12
Namizn <leleo2006@outlook.com>
2024-09-10 10:23:42 -0300

    TEstando

--- Prova Recuperação/Assets/Scripts/GameManager.cs
+++ Prova Recuperação/Assets/Scripts/GameManager.cs
@@ -7,12 +7,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Player()
+    {
+
+
+
     }
 }
